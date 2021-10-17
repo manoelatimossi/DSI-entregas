ee880f69e89419b7a7e2447efa00ca79bf31b82e
manoelatimossi <manoelatimossi1@gmail.com>
2021-10-17 20:06:41 -0300

    entrega 2 atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,163 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bebendo vinho',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Bebendo vinho'),
-    );
+        title: 'WordPair',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Wordpair());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyHomePage extends State<Wordpair> {
+  MyHomePage({Key? key, required this.title});
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _saved = <DocumentSnapshot>{};
+  final _suggestions = <DocumentSnapshot>[];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
-
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Você bebeu o vinho',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline1,
-            ),
-          ],
+      appBar: AppBar(title: Text("WordPair"), actions: [
+        IconButton(
+          icon: const Icon(Icons.list),
+          onPressed: () {
+            _pushSaved(context);
+          },
+          tooltip: 'Saved Suggestions',
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.wine_bar_sharp),
-
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ]),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('wordpair').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return ListView.builder(
+                itemExtent: 80.0,
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  _suggestions.addAll(snapshot.data!.docs);
+                  return _buildRow(snapshot.data!.docs[index], index);
+                });
+          }),
     );
   }
+
+  Widget _buildRow(DocumentSnapshot pair, index) {
+    final alreadySaved = _saved.contains(_suggestions[index]);
+    final item = _suggestions[index].get('name');
+    return Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0),
+            child: Icon(Icons.delete, color: Colors.white),
+          ),
+        ),
+        onDismissed: (direction) {
+          setState(() {
+            _suggestions.removeAt(index);
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('MaterialPageRoute index: $index')));
+        },
+        child: ListTile(
+            title: ListTile(title: Text('$item')),
+            onTap: () => edit(_suggestions, index, context),
+            trailing: Column(
+              children: <Widget>[
+                Container(
+                  child: IconButton(
+                    icon: Icon(
+                      alreadySaved ? Icons.favorite : Icons.favorite_border,
+                      color: alreadySaved ? Colors.red : null,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (alreadySaved) {
+                          _saved.remove(_suggestions[index]);
+                        } else {
+                          _saved.add(_suggestions[index]);
+                        }
+                      });
+                    },
+                  ),
+                )
+              ],
+            )));
+  }
+
+  void _pushSaved(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (DocumentSnapshot pair) {
+              return ListTile(
+                title: Text(
+                  pair.get('name'),
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  void edit(List<DocumentSnapshot> _suggestions, int index, context) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('Edit Suggestions'),
+              ),
+              body: TextFormField(
+                  initialValue: _suggestions[index].get('name'),
+                  onFieldSubmitted: (value) => setState(() {
+                        var aux = _suggestions[index].get('name');
+                        var aux2 = value;
+                        FirebaseFirestore.instance
+                            .collection('wordpair')
+                            .doc('name')
+                            .update({'name': aux});
+                        _suggestions[index]
+                            .reference
+                            .update(<String, dynamic>{aux: aux2});
+                      })));
+        },
+      ),
+    );
+  }
+}
+
+class Wordpair extends StatefulWidget {
+  @override
+  State<Wordpair> createState() => MyHomePage(title: 'WordPair');
 }
